4e2ff3390c6041881103bd817009754dc2236155
misha <golovchuk.misha@gmail.com>
2021-12-26 14:54:41 +0200

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const About = () => (
+  <div className="jumbotron">
+    <div className="container">
+      <h1 className="display-4">Моё React приложение</h1>
+      <p className="lead"> Версия приложения 1.0.1 </p>
+    </div>
+  </div>
+);
